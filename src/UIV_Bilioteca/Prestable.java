7455fb6f514d31683130577d2ffe932811bdb771
joshu <joshu@192.168.1.66>
2019-04-10 23:43:56 -0600

    CUERPO DE INTERFAZ

--- src/UIV_Bilioteca/Prestable.java
+++ src/UIV_Bilioteca/Prestable.java
@@ -1,7 +1,14 @@
+//DEFINICION DE INTERFAZ PRESTABLE
+//MARTINEZ ESTRADA JOSHUA
+//18111962
 
 package UIV_Bilioteca;
 
-
 public interface Prestable {
     
+    //METODOS ABSTRACTOS
+    public abstract void prestar();
+    public abstract void devolver();
+    public abstract void multa(int dias);
+    public abstract void prestado();
 }
