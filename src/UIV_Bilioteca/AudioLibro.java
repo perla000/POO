680537e2b2c90c7def4c7620d3d3e595cc9e18b8
joshu <joshu@192.168.1.66>
2019-04-11 21:36:09 -0600

    DEFINICION DE AUDIO LIBRO

--- src/UIV_Bilioteca/AudioLibro.java
+++ src/UIV_Bilioteca/AudioLibro.java
@@ -1,7 +1,63 @@
+//DEFINICION DE LA CLASE AUDIO LIBRO (PUBLICACIONES)
+//MARTINEZ ESTRADA JOSHUA
+//18111962
 
 package UIV_Bilioteca;
 
-
 public class AudioLibro extends Publicaciones{
     
+    //ATRIBUTOS
+    private int capacidad;
+    private int duracion;
+
+    //CONSTRUCTORES
+    public AudioLibro() {
+        super();
+        this.capacidad = 0;
+        this.duracion = 0;
+    }
+    public AudioLibro(int capacidad, int duracion, String titulo, String editorial, int aPub, String codigo) {
+        super(titulo, editorial, aPub, codigo);
+        this.capacidad = capacidad;
+        this.duracion = duracion;
+    }
+
+    @Override
+    public String toString() {
+        return "AudioLibro{" + "capacidad=" + capacidad + ", duracion=" + duracion + '}';
+    }
+
+    //SETTERS
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    //GETTERS
+    public int getCapacidad() {
+        return capacidad;
+    }
+    public int getDuracion() {
+        return duracion;
+    }
+    
+    //IMPLEMENTACION DE METODOS ABSTRACTOS
+    @Override
+    public void prestar() {
+        
+    }
+    @Override
+    public void devolver() {
+        
+    }
+    @Override
+    public void multa(int dias) {
+        
+    }
+    @Override
+    public void prestado() {
+        
+    }
 }
