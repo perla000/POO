ffabf22d644f8ee5de6b0d82c658707e5d7bc76a
joshu <joshu@192.168.1.66>
2019-04-11 21:38:46 -0600

    DEFINICION DE LIBRO FISICO

--- src/UIV_Bilioteca/LibroFisico.java
+++ src/UIV_Bilioteca/LibroFisico.java
@@ -1,7 +1,54 @@
+//DEFINICION DE CLASE LIBRO FISICO (PUBLICACION)
+//MARTINEZ ESTRADA JOSHUA
+//18111962
 
 package UIV_Bilioteca;
 
-
 public abstract class LibroFisico extends Publicaciones{
     
+    //ATRIBUTOS
+    protected int numPag;
+    protected Fecha fechaPub;
+    protected boolean prestado;
+
+    //CONSTRUCTORES
+    public LibroFisico() {
+        super();
+        this.numPag = 0;
+        this.fechaPub = new Fecha();
+        this.prestado = false;
+    }
+    public LibroFisico(int numPag, Fecha fechaPub, boolean prestaso, String titulo, String editorial, int aPub, String codigo) {
+        super(titulo, editorial, aPub, codigo);
+        this.numPag = numPag;
+        this.fechaPub = fechaPub;
+        this.prestado = prestaso;
+    }
+
+    @Override
+    public String toString() {
+        return "LibroFisico{" + "numPag=" + numPag + ", fechaPub=" + fechaPub + ", prestaso=" + prestado + '}';
+    }
+
+    //SETTERS
+    public void setNumPag(int numPag) {
+        this.numPag = numPag;
+    }
+    public void setFechaPub(Fecha fechaPub) {
+        this.fechaPub = fechaPub;
+    }
+    public void setPrestaso(boolean prestado) {
+        this.prestado = prestado;
+    }
+
+    //GETTERS
+    public int getNumPag() {
+        return numPag;
+    }
+    public Fecha getFechaPub() {
+        return fechaPub;
+    }
+    public boolean isPrestado() {
+        return prestado;
+    }
 }
